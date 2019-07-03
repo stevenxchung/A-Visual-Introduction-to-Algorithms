9fa74a4f1b8a30357a70843a5eeecaf227c86291
Steven <stevenxchung@gmail.com>
2019-07-03 17:42:55 -0400

    Update 02-find-minimum-in-subarray.js

--- 04-selection-sort/02-find-minimum-in-subarray.js
+++ 04-selection-sort/02-find-minimum-in-subarray.js
@@ -14,3 +14,38 @@ var indexOfMinimum = function(array, startIndex) {
 
   return minIndex;
 };
+
+// Test
+console.log(indexOfMinimum([18, 6, 66, 44, 9, 22, 14, 5], 2));
+console.log(
+  indexOfMinimum(
+    [
+      2,
+      3,
+      5,
+      7,
+      11,
+      13,
+      17,
+      19,
+      23,
+      29,
+      31,
+      37,
+      41,
+      43,
+      47,
+      13,
+      59,
+      61,
+      67,
+      71,
+      73,
+      79,
+      83,
+      89,
+      97
+    ],
+    5
+  )
+);
