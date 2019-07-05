82582eab17ef68bbd6b31dbbde7aa8c3845cd144
Steven <stevenxchung@gmail.com>
2019-07-05 19:00:45 -0400

    Update 02-recursive-factorial.js

--- 06-recursion-algorithms/02-recursive-factorial.js
+++ 06-recursion-algorithms/02-recursive-factorial.js
@@ -7,3 +7,9 @@ var factorial = function(n) {
     return factorial(n - 1) * n;
   }
 };
+
+// Test
+console.log(factorial(0))
+console.log(factorial(1))
+console.log(factorial(2))
+console.log(factorial(5))
