f2aa75ef5369a5f8cb16f650b06177f8609cebb8
Steven <stevenxchung@gmail.com>
2019-07-07 07:47:00 -0400

    Update 02-recursive-factorial.js

--- 06-recursion-algorithms/02-recursive-factorial.js
+++ 06-recursion-algorithms/02-recursive-factorial.js
@@ -1,9 +1,9 @@
 var factorial = function(n) {
-  // base case: if n = 0, return 1
+  // Base case: if n = 0, return 1
   if (n === 0) {
     return 1;
   } else {
-    // recursive case: otherwise return factorial(n - 1) multiplied by n
+    // Recursive case: otherwise return factorial(n - 1) multiplied by n
     return factorial(n - 1) * n;
   }
 };
