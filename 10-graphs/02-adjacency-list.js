459b7d783ef59f9eb614dc0e0a480f7d43b6e89b
Steven <stevenxchung@gmail.com>
2019-07-10 08:57:02 -0400

    Update 02-adjacency-list.js

--- 10-graphs/02-adjacency-list.js
+++ 10-graphs/02-adjacency-list.js
@@ -1,4 +1,5 @@
 var edgeList = [[0, 2], [1, 3], [2, 3], [2, 4], [3, 5], [4, 5]];
 
 // Fill in this adjList to represent the graph
+// Similar to adjacency matrix but only listing the vertices attached to each edge
 var adjList = [[2], [3], [3, 4], [5], [5], []];
